a9dee6336dcdc969d867fc48e1dbe1307ca5e5ae
Chiragkhs-UX <chirragk@gmail.com>
2026-08-05 12:41:59 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Includes src folder if your files are inside /src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
